52bb577b1cb589d92f44189df666bd699e33a8a3
qaisbaraki81@gmail.com <qaisbaraki81@gmail.com>
2024-10-21 23:05:48 +0100

    first commit

--- terraform/.modules/selfhost/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "vm_id" {
-  description = "The ID of the created virtual machine"
-  value       = azurerm_virtual_machine.vm.id
-}
-
-output "vm_identity" {
-  value = azurerm_virtual_machine.vm.identity[0].principal_id
-}
